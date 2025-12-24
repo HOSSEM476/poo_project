137889236368f78da4c113b0a8fd1bfaf3b7f179
HP <HP@DESKTOP-084IFNI>
2025-12-24 23:55:57 +0100

    Intial Commit.

--- GamePOO/src/buildings/Mine.java
+++ GamePOO/src/buildings/Mine.java
@@ -1,5 +1,19 @@
 package buildings;
 
-public class Mine {
-
+public class Mine extends Building {
+    
+    private int goldProduction;
+    
+    public Mine() {
+        super("Mine", 100);
+        this.goldProduction = 15;
+    }
+    
+    public void produce(Building Building) {
+        System.out.println(getName() + " produit " + goldProduction + " or " );
+    }
+    
+    public int getGoldProduction() {
+        return goldProduction;
+    }
 }
