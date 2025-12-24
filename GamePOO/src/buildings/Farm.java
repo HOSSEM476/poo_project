7ba62e91a6e1385806a5b0bc5e9f646ec8f43512
HP <HP@DESKTOP-084IFNI>
2025-12-25 00:10:09 +0100

    Initial Commit.

--- GamePOO/src/buildings/Farm.java
+++ GamePOO/src/buildings/Farm.java
@@ -1,5 +1,20 @@
 package buildings;
 
-public class Farm {
 
+public class Farm extends Building {
+    
+    private int foodProduction;
+    
+    public Farm() {
+        super("Ferme", 50);
+        this.foodProduction = 20;
+    }
+    
+    public void produce( Building Building) {
+        System.out.println(getName() + " produit " + foodProduction + " nourriture " );
+    }
+    
+    public int getFoodProduction() {
+        return foodProduction;
+    }
 }
